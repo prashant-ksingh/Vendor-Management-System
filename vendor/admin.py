80116c7c8466eafb1e053be2b9a757822180914f
unknown <prashantkumarsingh934@gmail.com>
2023-12-12 00:34:47 +0530

    models and admin done

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Vendor,PurchaseOrder,HistoricalPerformance
 
-# Register your models here.
+admin.register(Vendor)
+admin.register(PurchaseOrder)
+admin.register(HistoricalPerformance)
