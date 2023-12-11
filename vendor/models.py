ec4c586471be5864fe063418594f6aa4053fb3fc
unknown <prashantkumarsingh934@gmail.com>
2023-12-12 00:38:28 +0530

    change in models

--- vendor/models.py
+++ vendor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Vendor(models.Model):
     name = models.CharField(max_length=255)
@@ -10,22 +11,31 @@ class Vendor(models.Model):
     average_response_time = models.FloatField(default=0.0)
     fulfillment_rate = models.FloatField(default=0.0)
 
+    def __str__(self):
+        return self.name
+
 class PurchaseOrder(models.Model):
     po_number = models.CharField(max_length=50, unique=True)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    order_date = models.DateTimeField()
+    order_date = models.DateTimeField(default=timezone.now)
     delivery_date = models.DateTimeField()
     items = models.JSONField()
     quantity = models.IntegerField()
     status = models.CharField(max_length=50)
     quality_rating = models.FloatField(null=True, blank=True)
-    issue_date = models.DateTimeField()
+    issue_date = models.DateTimeField(default=timezone.now)
     acknowledgment_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.po_number
 
 class HistoricalPerformance(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=timezone.now)
     on_time_delivery_rate = models.FloatField()
     quality_rating_avg = models.FloatField()
     average_response_time = models.FloatField()
     fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return f"{self.vendor.name} - {self.date}"
